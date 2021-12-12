8699e27f3cd89081a801368d0a95d2985a2db800
Saru Thuraiman <sthuraisingh@bloomberg.net>
2021-12-11 22:06:16 -0500

    Display pieces
     * Render each of the pieces in the starting board
    
     * I added a funciton to generate alt name for the images

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-export default function Piece({ piece }) {
+export default function Piece({ piece: { type, color } }) {
+    function pieceAltText(type, color) {
+        const pieceTypeText = {
+            b: "Bishop",
+            k: "King",
+            n: "Knight",
+            p: "Pawn",
+            q: "Queen",
+            r: "Rook"
+        }
+        const pieceColorText = {
+            b: "Black",
+            w: "White"
+        }
+        return pieceColorText[color] + " " + pieceTypeText[type];
+    }
+
+    const pieceImage = require(`./assets/${type}_${color}.png`);
     return (
-        <div>
-            {piece.type} , {piece.color}
+        <div className="piece-container">
+            <img src={pieceImage} alt={pieceAltText(type, color)} className="piece" />
         </div>
     )
 }
