f99d3245041341826c12d6986c2b10ef682041f9
Saru Thuraiman <sthuraisingh@bloomberg.net>
2021-12-11 22:47:34 -0500

    DragPreview show piece image rather than whole square

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDrag } from 'react-dnd';
+import { useDrag, DragPreviewImage } from 'react-dnd';
 
 export default function Piece({ piece: { type, color } }) {
     function pieceAltText(type, color) {
@@ -21,13 +21,26 @@ export default function Piece({ piece: { type, color } }) {
     const pieceImage = require(`./assets/${type}_${color}.png`);
 
     // https://react-dnd.github.io/react-dnd/docs/api/use-drag
-    const [, dragSource,] = useDrag({
+    /**
+     * react-dnd automatically screenshots the component being used
+     * as the preview. But that includes the backgroud of the square.
+     * If you move the dragSource to the <img>, due to screenshotting
+     * you will see that preview will include the image with transparency
+     * replaced with square background color.
+     *
+     * So we use DragPreviewImage to use same piece image to provide
+     * transparency.
+     */
+    const [, dragSource, dragPreview] = useDrag({
         type: 'piece',
         id: `${type}_${color}`
     })
     return (
-        <div className="piece-container" ref={dragSource}>
-            <img src={pieceImage} alt={pieceAltText(type, color)} className="piece" />
-        </div>
+        <>
+            <DragPreviewImage connect={dragPreview}></DragPreviewImage>
+            <div className="piece-container" ref={dragPreview} src={pieceImage}>
+                <img src={pieceImage} alt={pieceAltText(type, color)} className="piece" />
+            </div>
+        </>
     )
 }
